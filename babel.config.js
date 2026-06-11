55ed9b57c8a4caf7b9d365ce79a47666c7eb65a5
Toumiconde <toumanyconde527@gmail.com>
2026-06-11 01:33:09 +0000

    fix: correct babel config for Reanimated 4 - worklets/plugin only

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,8 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // react-native-worklets/plugin MUST be listed before react-native-reanimated/plugin
+      // react-native-worklets/plugin must be listed LAST (Reanimated 4.x)
       'react-native-worklets/plugin',
-      'react-native-reanimated/plugin',
     ],
   };
 };
